577e51b873d3e603f25c20689f6d004419aa81e5
Javier Romero <javier.romero@tuebingen.mpg.de>
2016-09-07 00:56:18 +0200

    fix indentation

--- structural_rnn/CRFProblems/H3.6m/generateMotionData.py
+++ structural_rnn/CRFProblems/H3.6m/generateMotionData.py
@@ -10,38 +10,40 @@ import socket as soc
 base_dir = open('basedir','r').readline().strip()
 
 def unNormalizeData(normalizedData,data_mean,data_std,dimensions_to_ignore):
-	T = normalizedData.shape[0]
-	D = data_mean.shape[0]
-	origData = np.zeros((T,D),dtype=np.float32)
-	dimensions_to_use = []
-	for i in range(D):
-		if i in dimensions_to_ignore:
-			continue
-		dimensions_to_use.append(i)
-	dimensions_to_use = np.array(dimensions_to_use)
+    T = normalizedData.shape[0]
+    D = data_mean.shape[0]
+    origData = np.zeros((T,D),dtype=np.float32)
+    dimensions_to_use = []
+    for i in range(D):
+        if i in dimensions_to_ignore:
+            continue
+        dimensions_to_use.append(i)
+    dimensions_to_use = np.array(dimensions_to_use)
 
-	if not len(dimensions_to_use) == normalizedData.shape[1]:
-		return []
-		
-	origData[:,dimensions_to_use] = normalizedData
-	
-	stdMat = data_std.reshape((1,D))
-	stdMat = np.repeat(stdMat,T,axis=0)
-	meanMat = data_mean.reshape((1,D))
-	meanMat = np.repeat(meanMat,T,axis=0)
-	origData = np.multiply(origData,stdMat) + meanMat
-	return origData
+    if not len(dimensions_to_use) == normalizedData.shape[1]:
+        return []
+        
+    origData[:,dimensions_to_use] = normalizedData
+    
+    stdMat = data_std.reshape((1,D))
+    stdMat = np.repeat(stdMat,T,axis=0)
+    meanMat = data_mean.reshape((1,D))
+    meanMat = np.repeat(meanMat,T,axis=0)
+    origData = np.multiply(origData,stdMat) + meanMat
+    return origData
 
 def convertAndSave(fname):
-	fpath = path_to_trajfiles + fname
-	if not os.path.exists(fpath):
-		return False
-	normalizedData = readCSVasFloat(fpath)
-	origData = unNormalizeData(normalizedData,data_mean,data_std,dimensions_to_ignore)
-	if len(origData) > 0:
-		fpath = path_to_trajfiles + fname
-		writeMatToCSV(origData,fpath)
-		return True
+    fpath = path_to_trajfiles + fname
+    print(fpath)
+    if not os.path.exists(fpath):
+        return False
+    normalizedData = readCSVasFloat(fpath)
+    origData = unNormalizeData(normalizedData,data_mean,data_std,dimensions_to_ignore)
+    if len(origData) > 0:
+        fpath = path_to_trajfiles + fname
+        writeMatToCSV(origData,fpath+'.csv')
+        return True
+
 all_checkpoints = ['srnn_walking',
                    'lstm3lr_walking',
                    'lstm3lr_smoking',
