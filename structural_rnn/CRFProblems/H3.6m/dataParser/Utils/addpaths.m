ff43d75cfd99661309eb31420a8b3a1bd44cd3a1
Javier Romero <javier.romero@tuebingen.mpg.de>
2016-09-07 01:48:13 +0200

    more paths updated. The last two related to the graham taylor mocap toolbox

--- structural_rnn/CRFProblems/H3.6m/dataParser/Utils/addpaths.m
+++ structural_rnn/CRFProblems/H3.6m/dataParser/Utils/addpaths.m
@@ -1,8 +1,8 @@
-addpath('../../h36devkit/H36M');
-addpath('../../h36devkit/H36MBL');
-addpath('../../h36devkit/H36MGUI');
-addpath(genpath('../../h36devkit/utils'));
-addpath(genpath('../../h36devkit/external_utils'));
-addpath(genpath('../../MocapToolBox/matlab'));
-addpath(genpath('../../NDLUTIL0p161'));
+addpath('/is/ps/shared/datasets/human3.6M/Code/H36M');
+addpath('/is/ps/shared/datasets/human3.6M/Code/H36MBL');
+addpath('/is/ps/shared/datasets/human3.6M/Code/H36MGUI');
+addpath(genpath('/is/ps/shared/datasets/human3.6M/Code/utils'));
+addpath(genpath('/is/ps/shared/datasets/human3.6M/Code/external_utils'));
+addpath(genpath('/Users/jromero/src/mocap/matlab'));
+addpath(genpath('/Users/jromero/src/HDM05-Parser/amc2mat/extern/NDLUTIL0p161'));
 addpath(genpath('../../mhmublv/Motion'));
