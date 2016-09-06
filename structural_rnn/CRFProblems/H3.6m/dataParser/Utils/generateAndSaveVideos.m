518e54534efd6f10b84a99119262d0a231c964a9
Javier Romero <javier.romero@tuebingen.mpg.de>
2016-09-07 01:50:29 +0200

    a couple of changes in the script so that it deals with data generated from provided checkpoints

--- structural_rnn/CRFProblems/H3.6m/dataParser/Utils/generateAndSaveVideos.m
+++ structural_rnn/CRFProblems/H3.6m/dataParser/Utils/generateAndSaveVideos.m
@@ -1,32 +1,24 @@
 function  generateAndSaveVideos(  )
     addpaths;
     close all;
-    names = dir;
+    %checkpoint = 'checkpoints_dra_T_150_bs_100_tg_10_ls_10_fc_10_demo';
+    %folder = '/Users/jromero/RNNexp/structural_rnn/h3.6m/';
+    folder = '/Users/jromero/RNNexp/structural_rnn/h3.6m/official_permotion/';
+    names = dir(folder);
     db = H36MDataBase.instance;
     for i = 1:numel(names)
         nm = names(i);
         if nm.isdir && ~strcmp(nm.name , '.') && ~strcmp(nm.name , '..')
             disp(nm.name);
             
-            if ~strcmp(nm.name,'checkpoints_dra_T_150_bs_100_tg_100_ls_512_fc_256_initial_lr_0.001_clipnorm_25.0_nschd_[250,500.0,1000.0,1300.0,2000.0,2500.0,3300.0]_nrate_[0.01,0.05,0.1,0.2,0.3,0.5,0.7]_decschd_[1500.0,4500.0]_decrate_[0.1,0.1]_fc_fs_final')
-                continue;
-            end;
-            
-            %if ~strcmp(nm.name,'checkpoints_lstm_T_150_bs_100_tg_100_initial_lr_0.001_clipnorm_25.0_nschd_[250,500.0,1000.0,1300.0,2000.0,2500.0,3300.0]_nrate_[0.01,0.05,0.1,0.2,0.3,0.5,0.7]_decschd_[1500.0,4500.0]_decrate_[0.1,0.1]_fs_final')
-            %    continue;
-            %end;
-            
-            %if ~strcmp(nm.name,'checkpoints_lstm_T_150_bs_100_tg_100_initial_lr_0.001_clipnorm_25.0_nschd_[250,500.0,1000.0,1300.0,2000.0,2500.0,3300.0]_nrate_[0.01,0.05,0.1,0.2,0.3,0.5,0.7]_decschd_[1500.0,4500.0]_decrate_[0.1,0.1]_fs_smoking')
-            %    continue;
-            %end;
-            
             %unix(['mkdir -p ',nm.name,'/videos']);
             for N = 0:23 %23
                 
-                playGeneratedMotion( nm.name,['ground_truth_forecast_N_',num2str(N),'.dat'],5,false );
-                %for iterations = 5000
-                %        playGeneratedMotion( nm.name,['forecast_iteration_',num2str(iterations),'_N_',num2str(N),'.dat'],5,false );              
-                %end;
+                %playGeneratedMotion( nm.name,['ground_truth_forecast_N_',num2str(N),'.dat'],5,false );
+                for iterations = 1250
+                        %playGeneratedMotion( [folder,nm.name],['forecast_iteration_',num2str(iterations),'_N_',num2str(N),'_conv.csv'],5,false );              
+                        playGeneratedMotion( [folder,nm.name],['forecast_N_',num2str(N),'_conv.csv'],5,false );              
+                end;
                 %figure; plotCells( N );
             end;
         end;
